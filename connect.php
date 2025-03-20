ed467f0330371946fcba83b4575c93360be80edc
Edo Riansyah <edoriansyah@gmail.com>
2025-03-20 14:55:33 +0700

    Add configuration and connection files for database setup

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost';
+$db = 'bookdb';
+$user = 'admin';
+$password = 'password';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // if ($pdo) {
+    //     echo "Connected to the $db database successfully!";
+    // }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
