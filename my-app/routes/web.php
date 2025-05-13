ae49d6134bddf3df252e9c6496859dc90735374b
Edo Riansyah <edoriansyah@gmail.com>
2025-05-13 23:00:14 +0700

    Add route for products to access ProductController index method

--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 'Ini adalah halaman about';
+});
+
+Route::get('/products', [ProductController::class, 'index']);
