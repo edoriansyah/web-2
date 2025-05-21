fb623462b9040d73c1d2815557222323a5833ada
Edo Riansyah <edoriansyah@gmail.com>
2025-05-21 16:15:50 +0700

    Update PeminjamanSeeder to replace 'keterangan' with 'keperluan' for consistency

--- room-booking/database/seeders/PeminjamanSeeder.php
+++ room-booking/database/seeders/PeminjamanSeeder.php
@@ -20,7 +20,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-07',
                 'jam_mulai' => '09:00:00',
                 'jam_akhir' => '11:00:00',
-                'keterangan' => 'Rapat Tim',
+                'keperluan' => 'Rapat Tim',
             ],
             [
                 'ruang_id' => 2,
@@ -28,7 +28,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-08',
                 'jam_mulai' => '13:00:00',
                 'jam_akhir' => '15:00:00',
-                'keterangan' => 'Presentasi Produk',
+                'keperluan' => 'Presentasi Produk',
             ],
             [
                 'ruang_id' => 3,
@@ -36,7 +36,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-09',
                 'jam_mulai' => '10:00:00',
                 'jam_akhir' => '12:00:00',
-                'keterangan' => 'Diskusi Proyek',
+                'keperluan' => 'Diskusi Proyek',
             ],
             [
                 'ruang_id' => 4,
@@ -44,7 +44,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-10',
                 'jam_mulai' => '14:00:00',
                 'jam_akhir' => '16:00:00',
-                'keterangan' => 'Rapat Strategi',
+                'keperluan' => 'Rapat Strategi',
             ],
             [
                 'ruang_id' => 5,
@@ -52,7 +52,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-11',
                 'jam_mulai' => '08:00:00',
                 'jam_akhir' => '10:00:00',
-                'keterangan' => 'Pelatihan Karyawan',
+                'keperluan' => 'Pelatihan Karyawan',
             ],
             [
                 'ruang_id' => 1,
@@ -60,7 +60,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-12',
                 'jam_mulai' => '11:00:00',
                 'jam_akhir' => '13:00:00',
-                'keterangan' => 'Rapat Evaluasi',
+                'keperluan' => 'Rapat Evaluasi',
             ],
             [
                 'ruang_id' => 2,
@@ -68,7 +68,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-13',
                 'jam_mulai' => '15:00:00',
                 'jam_akhir' => '17:00:00',
-                'keterangan' => 'Koordinasi Tim',
+                'keperluan' => 'Koordinasi Tim',
             ],
             [
                 'ruang_id' => 3,
@@ -76,7 +76,7 @@ class PeminjamanSeeder extends Seeder
                 'tanggal' => '2025-05-14',
                 'jam_mulai' => '09:00:00',
                 'jam_akhir' => '11:00:00',
-                'keterangan' => 'Rapat Anggaran',
+                'keperluan' => 'Rapat Anggaran',
             ],
         ]);
     }
