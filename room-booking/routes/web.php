c4146da507a294117a28f4929a1d9109103c346e
Edo Riansyah <edoriansyah@gmail.com>
2025-05-23 09:18:45 +0700

    Refactor routes to organize Livewire components for Ruang and Counter

--- room-booking/routes/web.php
+++ room-booking/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
+use App\Livewire\Counter;
+use App\Livewire\Ruang\EditRuang;
+use App\Livewire\Ruang\ListRuang;
 use App\Livewire\Settings\Profile;
+use App\Livewire\Ruang\CreateRuang;
+use App\Livewire\Settings\Password;
+use App\Livewire\Settings\Appearance;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +23,12 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+
+    Route::get('/counter', Counter::class);
+
+    Route::get('/ruang', ListRuang::class)->name('ruang.index');
+    Route::get('/ruang/create', CreateRuang::class)->name('ruang.create');
+    Route::get('/ruang/edit/{ruang}', EditRuang::class)->name('ruang.edit');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
